03dc8dd136caee29eba07a6060529917c9cd9cc4
athosps <thanpart@hotmail.com>
2025-02-01 10:09:20 +0000

    Update Set-OUPermissions.ps1

--- Scripts/Set-OUPermissions.ps1
+++ Scripts/Set-OUPermissions.ps1
@@ -16,45 +16,45 @@ Author - Johan Arwidmark
     Twitter: @jarwidmark
     Blog   : http://deploymentresearch.com
 #>
-
 Param
 (
-[parameter(mandatory=$true,HelpMessage="Please, provide the account name.")][ValidateNotNullOrEmpty()]$Account,
-[parameter(mandatory=$true,HelpMessage="Please, provide the target OU.")][ValidateNotNullOrEmpty()]$TargetOU
+    [parameter(mandatory=$true,HelpMessage="Please, provide a name.")][ValidateNotNullOrEmpty()]$Account,
+    [parameter(mandatory=$true,HelpMessage="Please, provide the organization unit(s) to be used.")][ValidateNotNullOrEmpty()]$TargetOUs
 )
 
 # Start logging to screen
 Write-host (get-date -Format u)" - Starting"
 
-# This i what we typed in
+# This is what we typed in
 Write-host "Account to search for is" $Account
-Write-Host "OU to search for is" $TargetOU
+Write-Host "OUs to search for are" $TargetOUs
 
-if ($TargetOU -like '*dc=*')
-{ 
-    Write-Warning "Oupps, only specify the OU path. We get the domain for you..."
-    Break
-} 
-
-$CurrentDomain = Get-ADDomain
-
-$OrganizationalUnitDN = $TargetOU+","+$CurrentDomain
+$CurrentDomain = (Get-ADDomain).DistinguishedName
 $SearchAccount = Get-ADUser $Account
-
 $SAM = $SearchAccount.SamAccountName
 $UserAccount = $CurrentDomain.NetBIOSName+"\"+$SAM
 
 Write-Host "Account is = $UserAccount"
-Write-host "OU is =" $OrganizationalUnitDN
 
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":CCDC;Computer" /I:T | Out-Null
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":LC;;Computer" /I:S | Out-Null
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":RC;;Computer" /I:S | Out-Null
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":WD;;Computer" /I:S  | Out-Null
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":WP;;Computer" /I:S  | Out-Null
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":RP;;Computer" /I:S | Out-Null
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":CA;Reset Password;Computer" /I:S | Out-Null
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":CA;Change Password;Computer" /I:S | Out-Null
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":WS;Validated write to service principal name;Computer" /I:S | Out-Null
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":WS;Validated write to DNS host name;Computer" /I:S | Out-Null
-dsacls.exe $OrganizationalUnitDN
+$OUs = $TargetOUs -split ";"
+
+foreach ($OU in $OUs) {
+    $OrganizationalUnitDN = $OU # Do not concatenate $CurrentDomain here
+
+    Write-host "Processing OU = " $OrganizationalUnitDN
+
+    dsacls.exe $OrganizationalUnitDN /G $UserAccount":CCDC;Computer" /I:T | Out-Null
+    dsacls.exe $OrganizationalUnitDN /G $UserAccount":LC;;Computer" /I:S | Out-Null
+    dsacls.exe $OrganizationalUnitDN /G $UserAccount":RC;;Computer" /I:S | Out-Null
+    dsacls.exe $OrganizationalUnitDN /G $UserAccount":WD;;Computer" /I:S  | Out-Null
+    dsacls.exe $OrganizationalUnitDN /G $UserAccount":WP;;Computer" /I:S  | Out-Null
+    dsacls.exe $OrganizationalUnitDN /G $UserAccount":RP;;Computer" /I:S | Out-Null
+    dsacls.exe $OrganizationalUnitDN /G $UserAccount":CA;Reset Password;Computer" /I:S | Out-Null
+    dsacls.exe $OrganizationalUnitDN /G $UserAccount":CA;Change Password;Computer" /I:S | Out-Null
+    dsacls.exe $OrganizationalUnitDN /G $UserAccount":WS;Validated write to service principal name;Computer" /I:S | Out-Null
+    dsacls.exe $OrganizationalUnitDN /G $UserAccount":WS;Validated write to DNS host name;Computer" /I:S | Out-Null
+    dsacls.exe $OrganizationalUnitDN
+}
+
+Write-host (get-date -Format u)" - Finished"
+
